8cd8b3fa6b06a81e9ec226fa12d5f178d826e805
dino <mvpstars@gmail.com>
2022-12-23 17:17:14 +0800

    FlexSendMessage

--- src/examples/flask-echo/app.py
+++ src/examples/flask-echo/app.py
@@ -236,7 +236,7 @@ def message_text(event):
 
         #line_bot_api.reply_message(event.reply_token, TextSendMessage('Come in 002' + message))
 
-        line_bot_api.reply_message(event.reply_token, buttons_template_message)
+        line_bot_api.reply_message(event.reply_token, flex_message)
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message + '???'))
 
