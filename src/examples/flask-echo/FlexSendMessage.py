0bc411170b2fe043e44e8e2f4d084726813dfc22
dino <mvpstars@gmail.com>
2022-12-26 15:33:29 +0800

    Update

---
+++ src/examples/flask-echo/FlexSendMessage.py
@@ -0,0 +1,250 @@
+# -*- coding: utf-8 -*-
+
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+import os
+import sys
+from argparse import ArgumentParser
+
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, MessageAction, TemplateSendMessage, CarouselTemplate, CarouselColumn, FlexSendMessage,
+)
+
+app = Flask(__name__)
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def message_text(event):
+    # basic
+    '''
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+    '''
+
+    # further
+    message = text=event.message.text
+    if '股票' in message:
+        #line_bot_api.reply_message(event.reply_token, TextSendMessage('Come in 001' + message))
+
+        flex_message = FlexSendMessage(
+            alt_text = '股票資訊',
+            contents = {
+                "type": "bubble",
+                "hero": {
+                    "type": "image",
+                    "url": "https://miro.medium.com/max/1838/1*xRW05xCHmq7r8OOmFzlosw.png",
+                    "size": "full",
+                    "aspectRatio": "20:13",
+                    "aspectMode": "cover",
+                    "action": {
+                    "type": "uri",
+                    "uri": "http://linecorp.com/"
+                    }
+                },
+                "body": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                    {
+                        "type": "text",
+                        "text": "Brown Cafe",
+                        "weight": "bold",
+                        "size": "xl"
+                    },
+                    {
+                        "type": "box",
+                        "layout": "baseline",
+                        "margin": "md",
+                        "contents": [
+                        {
+                            "type": "icon",
+                            "size": "sm",
+                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                        },
+                        {
+                            "type": "icon",
+                            "size": "sm",
+                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                        },
+                        {
+                            "type": "icon",
+                            "size": "sm",
+                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                        },
+                        {
+                            "type": "icon",
+                            "size": "sm",
+                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                        },
+                        {
+                            "type": "icon",
+                            "size": "sm",
+                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+                        },
+                        {
+                            "type": "text",
+                            "text": "4.0",
+                            "size": "sm",
+                            "color": "#999999",
+                            "margin": "md",
+                            "flex": 0
+                        }
+                        ]
+                    },
+                    {
+                        "type": "box",
+                        "layout": "vertical",
+                        "margin": "lg",
+                        "spacing": "sm",
+                        "contents": [
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "spacing": "sm",
+                            "contents": [
+                            {
+                                "type": "text",
+                                "text": "Place",
+                                "color": "#aaaaaa",
+                                "size": "sm",
+                                "flex": 1
+                            },
+                            {
+                                "type": "text",
+                                "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                                "wrap": True,
+                                "color": "#666666",
+                                "size": "sm",
+                                "flex": 5
+                            }
+                            ]
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "spacing": "sm",
+                            "contents": [
+                            {
+                                "type": "text",
+                                "text": "Time",
+                                "color": "#aaaaaa",
+                                "size": "sm",
+                                "flex": 1
+                            },
+                            {
+                                "type": "text",
+                                "text": "10:00 - 23:00",
+                                "wrap": True,
+                                "color": "#666666",
+                                "size": "sm",
+                                "flex": 5
+                            }
+                            ]
+                        }
+                        ]
+                    }
+                    ]
+                },
+                "footer": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "contents": [
+                    {
+                        "type": "button",
+                        "style": "link",
+                        "height": "sm",
+                        "action": {
+                        "type": "uri",
+                        "label": "CALL",
+                        "uri": "https://linecorp.com"
+                        }
+                    },
+                    {
+                        "type": "button",
+                        "style": "link",
+                        "height": "sm",
+                        "action": {
+                        "type": "uri",
+                        "label": "WEBSITE",
+                        "uri": "https://linecorp.com"
+                        }
+                    },
+                    {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [],
+                        "margin": "sm"
+                    }
+                    ],
+                    "flex": 0
+                }
+            }
+        )
+
+        #line_bot_api.reply_message(event.reply_token, TextSendMessage('Come in 002' + message))
+
+        line_bot_api.reply_message(event.reply_token, flex_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message + '???'))
+
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', default=8000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+
+    app.run(debug=options.debug, port=options.port)
--- src/examples/flask-echo/app.py
+++ src/examples/flask-echo/app.py
@@ -24,7 +24,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, MessageAction, TemplateSendMessage, CarouselTemplate, CarouselColumn, FlexSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, MessageAction, TemplateSendMessage, CarouselTemplate, CarouselColumn,
 )
 
 app = Flask(__name__)
@@ -76,165 +76,62 @@ def message_text(event):
     if '股票' in message:
         #line_bot_api.reply_message(event.reply_token, TextSendMessage('Come in 001' + message))
 
-        flex_message = FlexSendMessage(
+        buttons_template_message = TemplateSendMessage(
             alt_text = '股票資訊',
-            contents = {
-                "type": "bubble",
-                "hero": {
-                    "type": "image",
-                    "url": "https://miro.medium.com/max/1838/1*xRW05xCHmq7r8OOmFzlosw.png",
-                    "size": "full",
-                    "aspectRatio": "20:13",
-                    "aspectMode": "cover",
-                    "action": {
-                    "type": "uri",
-                    "uri": "http://linecorp.com/"
-                    }
-                },
-                "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                    {
-                        "type": "text",
-                        "text": "Brown Cafe",
-                        "weight": "bold",
-                        "size": "xl"
-                    },
-                    {
-                        "type": "box",
-                        "layout": "baseline",
-                        "margin": "md",
-                        "contents": [
-                        {
-                            "type": "icon",
-                            "size": "sm",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                        },
-                        {
-                            "type": "icon",
-                            "size": "sm",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                        },
-                        {
-                            "type": "icon",
-                            "size": "sm",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                        },
-                        {
-                            "type": "icon",
-                            "size": "sm",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                        },
-                        {
-                            "type": "icon",
-                            "size": "sm",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                        },
-                        {
-                            "type": "text",
-                            "text": "4.0",
-                            "size": "sm",
-                            "color": "#999999",
-                            "margin": "md",
-                            "flex": 0
-                        }
+            template = CarouselTemplate(
+                columns = [
+                    CarouselColumn(
+                        thumbnail_image_url = 'https://miro.medium.com/max/1838/1*xRW05xCHmq7r8OOmFzlosw.png',
+                        title = message + '股票資訊',
+                        text = '請點選請查詢的股票資訊',
+                        actions = [
+                            MessageAction(
+                                label = 'xxx' + ' 個股資訊',
+                                text = '個股資訊 ' + 'xxx'
+                            ),
+                            MessageAction(
+                                label = 'xxx' + ' 個股新聞',
+                                text = '個股新聞 ' + 'xxx'
+                            )
                         ]
-                    },
-                    {
-                        "type": "box",
-                        "layout": "vertical",
-                        "margin": "lg",
-                        "spacing": "sm",
-                        "contents": [
-                        {
-                            "type": "box",
-                            "layout": "baseline",
-                            "spacing": "sm",
-                            "contents": [
-                            {
-                                "type": "text",
-                                "text": "Place",
-                                "color": "#aaaaaa",
-                                "size": "sm",
-                                "flex": 1
-                            },
-                            {
-                                "type": "text",
-                                "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                                "wrap": True,
-                                "color": "#666666",
-                                "size": "sm",
-                                "flex": 5
-                            }
-                            ]
-                        },
-                        {
-                            "type": "box",
-                            "layout": "baseline",
-                            "spacing": "sm",
-                            "contents": [
-                            {
-                                "type": "text",
-                                "text": "Time",
-                                "color": "#aaaaaa",
-                                "size": "sm",
-                                "flex": 1
-                            },
-                            {
-                                "type": "text",
-                                "text": "10:00 - 23:00",
-                                "wrap": True,
-                                "color": "#666666",
-                                "size": "sm",
-                                "flex": 5
-                            }
-                            ]
-                        }
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url = 'https://miro.medium.com/max/1838/1*xRW05xCHmq7r8OOmFzlosw.png',
+                        title = message + '股票資訊',
+                        text = '請點選請查詢的股票資訊',
+                        actions = [
+                            MessageAction(
+                                label = 'xxx' + ' 最新分鐘圖',
+                                text = '最新分鐘圖 ' + 'xxx'
+                            ),
+                            MessageAction(
+                                label = 'xxx' + ' 日線圖',
+                                text = '日線圖 ' + 'xxx'
+                            )
                         ]
-                    }
-                    ]
-                },
-                "footer": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "spacing": "sm",
-                    "contents": [
-                    {
-                        "type": "button",
-                        "style": "link",
-                        "height": "sm",
-                        "action": {
-                        "type": "uri",
-                        "label": "CALL",
-                        "uri": "https://linecorp.com"
-                        }
-                    },
-                    {
-                        "type": "button",
-                        "style": "link",
-                        "height": "sm",
-                        "action": {
-                        "type": "uri",
-                        "label": "WEBSITE",
-                        "uri": "https://linecorp.com"
-                        }
-                    },
-                    {
-                        "type": "box",
-                        "layout": "vertical",
-                        "contents": [],
-                        "margin": "sm"
-                    }
-                    ],
-                    "flex": 0
-                }
-            }
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url = 'https://miro.medium.com/max/1838/1*xRW05xCHmq7r8OOmFzlosw.png',
+                        title = message + '股票資訊',
+                        text = '請點選請查詢的股票資訊',
+                        actions = [
+                            MessageAction(
+                                label = 'xxx' + ' 平均股利',
+                                text = '平均股利 ' + 'xxx'
+                            ),
+                            MessageAction(
+                                label = 'xxx' + ' 歷年股利',
+                                text = '歷年股利' + 'xxx'
+                            )
+                        ]
+                    )
+                ]
+            )
         )
 
         #line_bot_api.reply_message(event.reply_token, TextSendMessage('Come in 002' + message))
 
-        line_bot_api.reply_message(event.reply_token, flex_message)
+        line_bot_api.reply_message(event.reply_token, buttons_template_message)
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message + '???'))
 
